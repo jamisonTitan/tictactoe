87e36d245c7e58929f3364965b42095cbe39582a
Titan <jamisontitan@gmail.com>
2018-12-13 18:14:20 -0800

    fixed lots of AI bugs

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ for(let i = 0; i < 3; i++){
     tileStates[i] = new Array(3);
 }
 
+
 function botMove() {
     let tileScores = [];
     for(let i = 0; i < 3; i++){
@@ -50,7 +51,7 @@ function botMove() {
                             tileScores[i][i] += 4
                         if(tileStates[i + 1] !== undefined){
                                 if(tileStates[i+1][i+1] === 'Move'){
-                                    tileScores[i+1][i+1] += 4;
+                                    tileScores[i+1][i+1] += 2;
                                 }
                             }
                         }
@@ -64,7 +65,7 @@ function botMove() {
                             tileScores[i][2 - i] += 4;
                             if(tileStates[i + 1] !== undefined){
                                 if(tileStates[i + 1][2 - (i + 1)] === 'Move'){
-                                    tileScores[i + 1][2 - (i + 1)] += 4;
+                                    tileScores[i + 1][2 - (i + 1)] += 2;
                                 }
                             }
                         }
@@ -74,6 +75,7 @@ function botMove() {
             }
         }
     }
+    let btnTilePos = { x: -1, y: -1};
     //find max score in tileScores
     let maxes = [];
     for(row of tileScores){
@@ -81,20 +83,75 @@ function botMove() {
     }
     let max = Math.max(...maxes);
 
-    for(let row = 0;row < tileScores.length; row++){ //loop through tiles
+    for(let row = 0; row < tileScores.length; row++){ //loop through tiles
         for(let col = 0; col < tileScores[row].length; col++){
                 //have bot pick one with highest score
                 if(tileScores[row][col] === max){
                     let tempBtnId = 
                         "#button".concat((((row + col) + row * 2) + 1).toString());
                     $(tempBtnId).html('O');
+                    btnTilePos.x = row;
+                    btnTilePos.y = col;
                     //break out of array
-                    row = tileScores.length;
+                    row = tileScores.length -1;
+                    console.table(tileScores);
+
                 }
             }
     }
 
-    console.table(tileScores);
+        //-- Check if bot has won--
+
+        //loop through states
+        for(let row = 0;row < tileStates.length; row++){
+            for(let col = 0; col < tileStates[row].length; col++){
+                /*read html of button and write it to corresponding
+                    tile state index */
+                let tempBtnId = 
+                    "#button".concat((((row + col) + row * 2) + 1).toString());
+                tileStates[row][col] = $(tempBtnId).html();
+                console.log(tileStates[row][col]);
+            }
+        }
+
+        //check row
+        for(let i = 0; i < 3; i++) {
+            if(tileStates[btnTilePos.x][i] !== 'O')
+                break;
+            if(i === 2)
+                alert('bot win');
+        }
+
+        //check column
+        for(let i = 0; i < 3; i++) {
+            if(tileStates[i][btnTilePos.y] !== 'O')
+                break; 
+            if(i === 2)
+                alert('bot win');
+        }
+
+        //check diagonal
+        if(btnTilePos.x === btnTilePos.y) { //check if on diagonal
+            for(let i = 0; i < 3; i++){
+                if(tileStates[i][i] !== 'O')
+                    break;
+                if(i === 2)
+                    alert('bot win');
+            }
+        }
+
+        //check oppisite diagonal
+        if(btnTilePos.x + btnTilePos.y === 2) { //check if on oppisite diagonal
+            for(let i = 0; i < 3; i++){
+                if(tileStates[i][2 - i] !== 'O') {
+                    break;
+                }
+                if(i === 2)
+                    alert('bot win');
+            }
+        }
+
+
 }
 
 function move(btnId) {
@@ -102,7 +159,7 @@ function move(btnId) {
     if($(btnId).html() === 'Move'){
         //move btn
         $(btnId).html('X');
-        //--check if anyone has won--
+        //--check if player has won--
 
         let btnTilePos = { x: -1, y: -1}
         //loop through states
@@ -141,23 +198,28 @@ function move(btnId) {
         if(btnTilePos.x === btnTilePos.y) { //check if on diagonal
             for(let i = 0; i < 3; i++){
                 if(tileStates[i][i] !== 'X')
-                    break
+                    break;
                 if(i === 2)
                     alert('win');
             }
         }
 
         //check oppisite diagonal
-        if(btnTilePos.x + btnTilePos.y === 2 === 2) { //check if on oppisite diagonal
+        if(btnTilePos.x + btnTilePos.y === 2) { //check if on oppisite diagonal
             for(let i = 0; i < 3; i++){
-                if(tileStates[i][2 - i] !== 'X')
-                    break
+                if(tileStates[i][2-i] !== 'X') {
+                    console.log("i is: " + i + "\n tile[i][2-i] is: " + tileStates[i][2-i]);
+                    break;
+                }
                 if(i === 2)
                     alert('win');
+                
             }
         }
 
         botMove();
+
+
 
     }else {
         //TODO error handling
